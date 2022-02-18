fd479032991424469e339976e435792f869e78e5
ssysm <contact@theeditorstudio.com>
2022-02-17 21:58:31 -0500

    init project

---
+++ index.js
@@ -0,0 +1,24 @@
+// * Imports
+const express = require("express"); // Imports Express's class definition
+const morgan = require("morgan"); // Imports Morgan's class definition
+const Blockchain = require("./src/blockchain");
+
+// Global variables
+global.difficulty = 5; // Difficulty to mine a particular block
+global.blockchain = new Blockchain(); // Our copy of the blockchain
+global.transactions = []; // Our current transactions
+
+// Initialize express's class object
+const app = express();
+// Tell Express to use Morgan for logging requests to the console
+app.use(morgan("dev")); // Pretty-print requests with the "dev" format
+require("./routes")(app);
+
+// Create the port number for the server to listen on
+const port = 8080; // See: Wikipedia's List of TCP and UDP port numbers
+
+// Configure our server to run
+app.listen(port, () => {
+  // Log that our server is running in the terminal
+  console.log(`Server is listening at http://localhost:${port}/`);
+});
---
+++ routes/mine.js
@@ -0,0 +1,16 @@
+// Mine block API
+function mine(app) {
+  app.get("/mine", (request, response) => {
+
+    // Add the block to our chain, which calls mine()
+    global.blockchain.addBlock();
+    // Clear our transactions
+    global.transactions = [];
+
+    // Send a success response
+    let msg = `Block added: ${global.blockchain.getLastBlock().prettify()}`;
+    response.status(200).send(msg);
+  });
+}
+
+module.exports = mine;
